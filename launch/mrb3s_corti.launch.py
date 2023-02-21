41c16cff78ae8ba016efd3a5eb2f5c2e1fd00607
James Goppert <james.goppert@gmail.com>
2023-02-21 12:16:01 -0500

    Update launch.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- launch/mrb3s_corti.launch.py
+++ launch/mrb3s_corti.launch.py
@@ -1,12 +1,21 @@
+from pathlib import Path
+
 import launch
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
     use_sim_time = True
     logger = launch.substitutions.LaunchConfiguration("log_level")
+    pkg_mrb3_description = Path(get_package_share_directory('mrb3_description'))
+
     return LaunchDescription([
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(
+            str(pkg_mrb3_description /  'launch/robot_description.launch.py'))),
         Node(
            package='corti',
            output='log',
@@ -45,8 +54,7 @@ def generate_launch_description():
            parameters=[
              {'use_sim_time': use_sim_time}
            ],
-           arguments=[
-               "0", "0", "0", "0", "0", "0",
-               "mrb3s/base_footprint", "mrb3s/RPLIDAR_A1M8/Base/lidar"]
+           arguments="--x 0 --y 0 --z 0 --roll 0 --pitch 0 --yaw 0 "
+           "--frame-id mrb3s/base_footprint --child-frame-id mrb3s/RPLIDAR_A1M8/Base/lidar".split()
         ),
     ])
