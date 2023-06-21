7e88015b65f7d067daff99ceec84dccba26da525
James Goppert <james.goppert@gmail.com>
2023-06-21 00:40:11 +0000

    Enable sync mode for odom_to_tf
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- src/odom_to_tf.cpp
+++ src/odom_to_tf.cpp
@@ -3,55 +3,91 @@
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/transform_broadcaster.h"
 #include "nav_msgs/msg/odometry.hpp"
+#include <nav_msgs/msg/detail/odometry__struct.hpp>
+#include <rclcpp/rate.hpp>
+
 
 class FramePublisher : public rclcpp::Node
 {
 public:
-  FramePublisher()
-  : Node("odom_to_tf")
-  {
-    // Initialize the transform broadcaster
-    tf_broadcaster_ =
-      std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+    FramePublisher()
+        : Node("odom_to_tf")
+    {
+        m_async = this->declare_parameter<bool>("async", false);
+        m_sync_dt = this->declare_parameter<double>("sync_dt", 0.02);
 
-    subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-      "/odom", 10,
-      std::bind(&FramePublisher::handle_odom, this, std::placeholders::_1));
-  }
+        m_tf_broadcaster =
+            std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
+        m_sub_odom = this->create_subscription<nav_msgs::msg::Odometry>(
+            "/odom", 10,
+            std::bind(&FramePublisher::handle_odom, this, std::placeholders::_1));
+
+        if (not m_async) {
+            m_timer = this->create_wall_timer(
+                std::chrono::duration<float>{m_sync_dt},
+                std::bind(&FramePublisher::on_timer, this));
+        }
+    }
 
 private:
-  void handle_odom(const std::shared_ptr<nav_msgs::msg::Odometry> msg)
-  {
-    geometry_msgs::msg::TransformStamped t;
+    void handle_odom(const std::shared_ptr<nav_msgs::msg::Odometry> msg)
+    {
+        if (m_async) {
+            publish_tf(msg);
+        } else {
+            m_odom = msg;
+        }
+    }
 
-    // header
-    t.header.stamp = this->get_clock()->now();
-    t.header.frame_id = msg->header.frame_id;
-    t.child_frame_id = msg->child_frame_id;
+    void on_timer() {
+        if (m_odom != NULL) {
+            publish_tf(m_odom);
+        }
+    }
 
-    // position
-    t.transform.translation.x = msg->pose.pose.position.x;
-    t.transform.translation.y = msg->pose.pose.position.y;
-    t.transform.translation.z = msg->pose.pose.position.z;
+    void publish_tf(const std::shared_ptr<nav_msgs::msg::Odometry>) {
+        geometry_msgs::msg::TransformStamped t;
 
-    // attitude
-    t.transform.rotation.x = msg->pose.pose.orientation.x;
-    t.transform.rotation.y = msg->pose.pose.orientation.y;
-    t.transform.rotation.z = msg->pose.pose.orientation.z;
-    t.transform.rotation.w = msg->pose.pose.orientation.w;
+        // header
+        if (m_async) {
+            t.header.stamp = m_odom->header.stamp;
+        } else {
+            t.header.stamp = this->get_clock()->now();
+        }
+        t.header.frame_id = m_odom->header.frame_id;
+        t.child_frame_id = m_odom->child_frame_id;
 
-    // Send the transformation
-    tf_broadcaster_->sendTransform(t);
-  }
+        // position
+        t.transform.translation.x = m_odom->pose.pose.position.x;
+        t.transform.translation.y = m_odom->pose.pose.position.y;
+        t.transform.translation.z = m_odom->pose.pose.position.z;
 
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
-  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+        // attitude
+        t.transform.rotation.x = m_odom->pose.pose.orientation.x;
+        t.transform.rotation.y = m_odom->pose.pose.orientation.y;
+        t.transform.rotation.z = m_odom->pose.pose.orientation.z;
+        t.transform.rotation.w = m_odom->pose.pose.orientation.w;
+
+        // Send the transformation
+        m_tf_broadcaster->sendTransform(t);
+    }
+
+    // attributes
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr m_sub_odom;
+    std::unique_ptr<tf2_ros::TransformBroadcaster> m_tf_broadcaster;
+    std::shared_ptr<nav_msgs::msg::Odometry> m_odom{NULL};
+    bool m_async;
+    float m_sync_dt;
+    rclcpp::TimerBase::SharedPtr m_timer;
 };
 
 int main(int argc, char * argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<FramePublisher>());
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<FramePublisher>());
+    rclcpp::shutdown();
+    return 0;
 }
+
+// vi: ts=4 sw=4 et
