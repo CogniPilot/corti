58d7c6c339f4ee101be7165d06e6f983226cd4d5
James Goppert <james.goppert@gmail.com>
2024-06-12 14:40:43 -0400

    Add rviz_model script.
    Signed-off-by: James Goppert <james.goppert@gmail.com>

--- scripts/rviz_model.py
+++ scripts/rviz_model.py
@@ -10,6 +10,9 @@ class ModelPublisher(Node):
 
     def __init__(self):
         super().__init__('model_publisher')
+        self.declare_parameter('model', rclpy.Parameter.Type.STRING)
+        self.declare_parameter('use_texture', True)
+
         self.publisher_ = self.create_publisher(Marker, 'city', 1)
         self.msg = Marker()
         self.msg.header.frame_id = 'map'
@@ -23,12 +26,30 @@ class ModelPublisher(Node):
         self.msg.scale.y = 1.0
         self.msg.scale.z = 1.0
         
-        self.msg.color.a = 1.0
-        self.msg.color.r = 0.0
-        self.msg.color.g = 0.0
-        self.msg.color.b = 1.0
-        self.msg.mesh_resource = "package://corti/meshes/city.dae"
+        use_texture = self.get_parameter('use_texture').value
+
+        if use_texture:
+            self.msg.mesh_use_embedded_materials = True
+        else:
+            self.msg.color.a = 0.1
+            self.msg.color.r = 0.0
+            self.msg.color.g = 0.0
+            self.msg.color.b = 1.0
+
+        try:
+            self.msg.mesh_resource = self.get_parameter('model').value
+        except rclpy.exceptions.ParameterUninitializedException as e:
+            self.get_logger().error(str(e))
+            rclpy.shutdown()
+            exit(1)
+
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
         self.publisher_.publish(self.msg)
+
 
 def main(args=None):
     rclpy.init(args=args)
